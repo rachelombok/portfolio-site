e03c1f89ffcf399caded37b33bd2df4681b35f57
Rachel Ombok <rachel.ombok@gmail.com>
2025-11-28 00:13:31 -0500

    remove image

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Link from "next/link";
-import Image from "next/image";
 import { useState } from "react";
 import SmallNavDirectory from "./SmallNavDirectory";
 import { cn } from "@/lib/utils";
