e5b3a812ceb17790289dce144c7b6f615c23be38
Rachel Ombok <rachel.ombok@gmail.com>
2026-01-25 22:01:06 -0500

    add new email provider

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,87 +1,46 @@
 "use client";
-import React, { useRef, useState } from "react";
-import emailjs from "@emailjs/browser";
+import React from "react";
+import { useForm, ValidationError } from '@formspree/react';
 
 const ContactForm: React.FC = () => {
-  const form = useRef<HTMLFormElement>(null);
-  const [formMsg, setFormMsg] = useState<string>("");
-
-  const sendEmail = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    if (form.current) {
-      try {
-        setFormMsg("");
-        await emailjs.sendForm(
-          process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!,
-          process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID!,
-          form.current,
-          process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY!
-        );
-        form.current.reset();
-        setFormMsg("Success!");
-      } catch (error) {
-        setFormMsg("Something went wrong. Please try again.");
-        console.error(error);
-      }
-    }
-  };
+  const [state, handleSubmit] = useForm(process.env.NEXT_PUBLIC_FORMSPREE_ID || '');
 
   return (
-    <form
-      ref={form}
-      onSubmit={sendEmail}
-      className="w-full max-w-3xl shadow-md rounded px-8 pt-6 pb-8 mb-4 mx-auto"
-    >
-      <label
-        htmlFor="name"
-        className="block text-gray-700 text-sm font-bold mb-2"
-      >
-        Name*
+    <form onSubmit={handleSubmit} className="w-full max-w-3xl shadow-md rounded px-8 pt-6 pb-8 mb-4 mx-auto">
+      <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">
+        Email Address
       </label>
       <input
-        type="text"
-        id="name"
-        name="name"
-        required
+        id="email"
+        type="email"
+        name="email"
         className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-4"
       />
-
-      <label
-        htmlFor="email"
-        className="block text-gray-700 text-sm font-bold mb-2"
-      >
-        Email*
-      </label>
-      <input
-        type="email"
-        id="email"
-        name="email"
-        required
-        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline mb-4"
+      <ValidationError
+        prefix="Email"
+        field="email"
+        errors={state.errors}
       />
-
-      <label
-        htmlFor="message"
-        className="block text-gray-700 text-sm font-bold mb-2"
-      >
-        Message*
+      <label htmlFor="message" className="block text-gray-700 text-sm font-bold mb-2">
+        Message
       </label>
       <textarea
         id="message"
         name="message"
-        required
-        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline mb-4"
+        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-4"
+      />
+      <ValidationError
+        prefix="Message"
+        field="message"
+        errors={state.errors}
       />
       <div className="flex items-center justify-between">
-        <button
-          type="submit"
-          className="bg-gray-400 hover:bg-pink-200 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline cursor-pointer"
-        >
-          Send
+        <button type="submit" disabled={state.submitting} className="bg-gray-400 hover:bg-pink-200 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline cursor-pointer">
+          Submit
         </button>
-        <p className="mx-4 font-semibold">{formMsg}</p>
+        <p className="mx-4 font-semibold">{state.succeeded ? "Success!" : state.submitting ? "Submitting..." : ""}</p>
       </div>
+
     </form>
   );
 };
